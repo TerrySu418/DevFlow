fc9ab9f6b164665cdcb3f6a1383dc4d41c648734
Terry_Su <sao111053@gmail.com>
2025-02-13 22:46:22 +0800

    Setup Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 const Home = () => {
   return (
-    <h1 className="background-light800_dark300 text-3xl font-black text-primary-500">
-      Welcome to the world of Next.js
-    </h1>
+    <>
+      <h1 className="background-light800_dark300 text-3xl font-black text-primary-500">
+        Welcome to the world of Next.js
+      </h1>
+      <h1 className="font-space-grotesk text-3xl">
+        Welcome to the world of Next.jsgg
+      </h1>
+    </>
   );
 };
 
