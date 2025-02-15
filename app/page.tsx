87b2f1e6e2418a2213991c38d18c26f119e802b7
Terry_Su <sao111053@gmail.com>
2025-02-15 12:23:34 +0800

    Installing ShadCN UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
+
 const Home = () => {
   return (
     <>
@@ -7,6 +18,18 @@ const Home = () => {
       <h1 className="font-space-grotesk text-3xl">
         Welcome to the world of Next.jsgg
       </h1>
+      <DropdownMenu>
+  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+
     </>
   );
 };
