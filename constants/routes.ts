196bc10597967afd99174c4bca15913276af28e4
Terry_Su <sao111053@gmail.com>
2025-03-02 17:24:36 +0800

    local searchbar

--- constants/routes.ts
+++ constants/routes.ts
@@ -4,6 +4,7 @@ const ROUTES = {
   SIGN_UP: "/sign-up",
   PROFILE: (_id: string) => `/profile/${_id}`,
   TAGS: (_id: string) => `/tags/${_id}`,
+  ASK_QUESTION: "/ask-question"
 };
 
 export default ROUTES;
