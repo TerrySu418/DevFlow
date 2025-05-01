26dacfdc66193f74086bde39495079bcdcbef050
Terry_Su <sao111053@gmail.com>
2025-05-01 10:26:52 +0800

    Implement getTags Server Action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { getTags } from "@/lib/actions/tag.action";
 
-const page = () => {
-  return <div>tags</div>;
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    // query: "mongo"
+  });
+
+  const { tags } = data || {};
+
+  console.log("TAGS", JSON.stringify(tags, null, 2));
+
+  return <div>Tag</div>
 };
 
-export default page;
+export default Tags;
