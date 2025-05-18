295bdaac606c4b7729e0969437d5fb55a164a5c5
Terry_Su <sao111053@gmail.com>
2025-05-18 19:31:20 +0800

    FIx Deplpoyment Issue

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,9 @@
-export { auth as middleware } from "@/auth"
+export { auth as middleware } from "@/auth";
+
+export const config = {
+  runtime: "nodejs", // Specify Node.js runtime
+  matcher: [
+    // Your matchers here
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
